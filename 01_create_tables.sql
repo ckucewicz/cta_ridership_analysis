55d827b4a7ceae1b66cf83f95820d670f683b47e
Chris Kucewicz <cfkucewicz@gmail.com>
2025-07-19 10:37:23 -0500

    Adding SQL script for table creation

---
+++ 01_create_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE ridership_L_stations (
+	station_id INT,
+	station_name VARCHAR(100),
+	ride_date DATE,
+	day_type VARCHAR(1),
+	rides INT
+);
+
+CREATE TABLE L_station_info (
+	stop_id INT PRIMARY KEY,
+	direction_id VARCHAR(1),
+	stop_name VARCHAR(100),
+	station_name VARCHAR(100),
+	station_descriptive_name VARCHAR(150),
+	map_id INT, 
+	ada_approved BOOLEAN,
+	red BOOLEAN, 
+	blue BOOLEAN,
+	green BOOLEAN,
+	brownn BOOLEAN,
+	purple BOOLEAN,
+	pexp BOOLEAN,
+	yellow BOOLEAN,
+	pnk BOOLEAN, 
+	orange BOOLEAN,
+	location TEXT
+);
